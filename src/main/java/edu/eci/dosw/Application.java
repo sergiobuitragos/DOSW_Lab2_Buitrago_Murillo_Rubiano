539a76b43e2cf2cc4d15230ea53978d682d20fb5
Miguel Angel Murillo Hurtado <miguel.murillo-h@mail.escuelaing.edu.co>
2026-08-10 09:09:24 -0500

    initial Maven project structure

---
+++ src/main/java/edu/eci/dosw/Application.java
@@ -0,0 +1,8 @@
+package edu.eci.dosw;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Maven project configured and running correctly.");
+    }
+}
