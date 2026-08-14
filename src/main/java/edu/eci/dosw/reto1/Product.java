833fe1f8e6cc0b7905117445d510a7c51a559697
Miguel Angel Murillo Hurtado <miguel.murillo-h@mail.escuelaing.edu.co>
2026-08-14 09:16:59 -0500

    feat: class Product

---
+++ src/main/java/edu/eci/dosw/reto1/Product.java
@@ -0,0 +1,50 @@
+package edu.eci.dosw.reto1;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * Represents a product in the system.
+ * Once created, the name and price cannot be changed 
+ */
+public final class Product {
+    private final String name;
+    private final BigDecimal unitPrice;
+
+    public Product(String name, BigDecimal unitPrice){
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("Product name cannot be null or blank");
+        }
+        if(unitPrice == null || unitPrice.signum() < 0){
+            throw new IllegalArgumentException("Product units cannot be null or negative");
+        }
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return name.equals(product.name) && unitPrice.equals(product.unitPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, unitPrice);
+    }
+
+    @Override
+    public String toString() {
+        return name + " (COP " + unitPrice + ")";
+    }
+}
