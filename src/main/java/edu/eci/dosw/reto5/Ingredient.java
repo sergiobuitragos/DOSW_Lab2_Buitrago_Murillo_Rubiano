6b925c810bcc1e6605d66a7bc1e7ccd841021718
Sergio Buitrago <sergio.buitrago@mail.escuelaing.edu.co>
2026-08-18 12:00:20 -0500

    se creo la interfaz para ingrediente

---
+++ src/main/java/edu/eci/dosw/reto5/Ingredient.java
@@ -0,0 +1,17 @@
+package edu.eci.dosw.reto5;
+
+/**
+ * es un ingrediente de mas para el cafe
+ */
+public interface Ingredient {
+
+    /**
+     * @return
+     */
+    String getName();
+
+    /**
+     * @return
+     */
+    double getPrice();
+}
