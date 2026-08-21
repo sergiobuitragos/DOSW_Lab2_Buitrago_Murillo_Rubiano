171f08bafe5cdc528948d5b003a2b6e846526ae5
RubianoJ <juanita.rubiano-f@mail.escuelaing.edu.co>
2026-08-20 22:51:44 -0500

    Conectar Application.java con demo de reto5

--- src/main/java/edu/eci/dosw/Application.java
+++ src/main/java/edu/eci/dosw/Application.java
@@ -4,5 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Maven project configured and running correctly.");
+
+        // Ejecuta la demo del reto5
+        edu.eci.dosw.reto5.Main.main(args);
     }
 }
