2456056ebea242f6a17a96cd3bdc2567b517e35a
Miguel Angel Murillo Hurtado <miguel.murillo-h@mail.escuelaing.edu.co>
2026-08-20 22:12:13 -0500

    feat: add Support Statistics and wire Reto 6 to application

--- src/main/java/edu/eci/dosw/Application.java
+++ src/main/java/edu/eci/dosw/Application.java
@@ -1,8 +1,11 @@
 package edu.eci.dosw;
 
+import edu.eci.dosw.reto6.Reto6;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Maven project configured and running correctly.");
+        Reto6.run();
     }
 }
