f4f0be82a6802e1cedee07dc14b5040539f2ec51
Miguel Angel Murillo Hurtado <miguel.murillo-h@mail.escuelaing.edu.co>
2026-08-20 21:09:07 -0500

    reto 2 invocado desde application

--- src/main/java/edu/eci/dosw/Application.java
+++ src/main/java/edu/eci/dosw/Application.java
@@ -1,8 +1,12 @@
 package edu.eci.dosw;
 
+import edu.eci.dosw.reto2.Reto2;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("Maven project configured and running correctly.");
+        Reto2.run();
     }
+
 }
