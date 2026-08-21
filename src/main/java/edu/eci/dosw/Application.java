2704a4d9f4446283b67283dddd7692fbde7386d1
RubianoJ <juanita.rubiano-f@mail.escuelaing.edu.co>
2026-08-20 22:02:12 -0500

    Conectar Application.java con demo de reto3

--- src/main/java/edu/eci/dosw/Application.java
+++ src/main/java/edu/eci/dosw/Application.java
@@ -4,5 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Maven project configured and running correctly.");
+
+        // Ejecuta la demo del reto3
+        edu.eci.dosw.reto3.Main.main(args);
     }
 }
